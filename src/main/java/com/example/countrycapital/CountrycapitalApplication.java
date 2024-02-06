33d1b5e846e93e19891b4e11f2b502319fe25906
Adnanrobi <adnanrobi6@gmail.com>
2024-02-07 04:00:49 +0600

    initializing the project with spring initializr

---
+++ src/main/java/com/example/countrycapital/CountrycapitalApplication.java
@@ -0,0 +1,13 @@
+package com.example.countrycapital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountrycapitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountrycapitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/countrycapital/CountrycapitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.countrycapital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountrycapitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
