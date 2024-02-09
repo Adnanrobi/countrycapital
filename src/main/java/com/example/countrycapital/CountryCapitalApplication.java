3fa6ec1a96a885032836b33e5f0856e4176c076e
Adnanrobi <adnanrobi6@gmail.com>
2024-02-10 00:42:09 +0600

    #updated Main Application Class

--- src/main/java/com/example/countrycapital/CountryCapitalApplication.java
+++ src/main/java/com/example/countrycapital/CountryCapitalApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CountrycapitalApplication {
+public class CountryCapitalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CountrycapitalApplication.class, args);
+		SpringApplication.run(CountryCapitalApplication.class, args);
 	}
 
 }
